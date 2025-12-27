8454653a1345d7f7e00fac9c26f0df222c7de0d2
Ardabiel <ardabili2017@gmail.com>
2025-12-27 18:21:12 +0700

    SPK-DSS

---
+++ spkk/spksaw-mas/layout/footer.php
@@ -0,0 +1,10 @@
+<footer>
+    <div class="footer clearfix mb-0 text-muted">
+        <div class="float-start">
+            <p></p>
+        </div>
+        <div class="float-end">
+            <p><span class="text-danger"><i class=""></i></span><agit add .>© 2025 Hilman. All rights reserved.</a></p>
+        </div>
+    </div>
+</footer>
